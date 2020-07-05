267c4e699f18ff7df565adab6b2cfbb799eac17b
rdsalakhov <salakhov.roman12@gmail.com>
2020-07-05 19:30:52 +0500

    Initial Commit

---
+++ RouteRecorder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RouteRecorder
+//
+//  Created by Роман Салахов on 05.07.2020.
+//  Copyright © 2020 Роман Салахов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
