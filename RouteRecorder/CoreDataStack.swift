fca98f2a15a7576c22965ebf1cccf28211c73226
rdsalakhov <salakhov.roman12@gmail.com>
2020-07-07 19:31:44 +0500

    added Core Data context

---
+++ RouteRecorder/CoreDataStack.swift
@@ -0,0 +1,39 @@
+//
+//  CoreDataStack.swift
+//  RouteRecorder
+//
+//  Created by Роман Салахов on 07.07.2020.
+//  Copyright © 2020 Роман Салахов. All rights reserved.
+//
+
+import CoreData
+
+class CoreDataStack {
+  
+  static let persistentContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: "RouteRecorder")
+    container.loadPersistentStores { (_, error) in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    }
+    return container
+  }()
+  
+  static var context: NSManagedObjectContext { return persistentContainer.viewContext }
+  
+  class func saveContext () {
+    let context = persistentContainer.viewContext
+    
+    guard context.hasChanges else {
+      return
+    }
+    
+    do {
+      try context.save()
+    } catch {
+      let nserror = error as NSError
+      fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+    }
+  }
+}
